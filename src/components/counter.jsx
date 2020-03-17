4925efcb34e5b9e2cda998204dc6b676166a3ab3
vm947 <vm947@nyu.edu>
2020-03-17 23:36:18 +0300

    still adding goodies

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,55 +2,62 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
-    /*imageURL: 'https://picsum.photos/200'
+    value: this.props.value
+  };
+
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement()} //event handlers use function reference, not a function with ()
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={this.props.onDelete}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
+
+/*imageURL: 'https://picsum.photos/200'
         this generates a random 200 by 200 pic.
         would reference image src as--> src={this.state.imageURL}
         */
-  };
 
-  /*constructor(){
+/*constructor(){
      super(); 
      this.handleIncrement = this.handleIncrement.bind(this);
      this is for binding EVENT handlers.
      You can use arrow function and not need to do this.
   };*/
 
-  /*styles = {
+/*styles = {
         fontSize: 50,
         fontWeight: 'bold'
         WE REFERENCE this.styles as an 
         attribute style-->style={this.styles};
     };*/
-
-  handleIncrement = () => {
-    this.setState({count: this.state.count + 1});
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button
-          onClick={this.handleIncrement} //event handlers use function reference, not a function with ()
-          className="btn btn-secondary btn-sm"
-        >
-          Increment
-        </button>
-      </React.Fragment>
-    );
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
-}
-
-export default Counter;
